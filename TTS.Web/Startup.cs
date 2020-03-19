7c51ca0f8b18647d50bafba87627f5e73c8592ea
Aibek Shulembekov <aibek1805@gmail.com>
2020-03-20 03:15:01 +0600

    ConnectionString stored to user-secrets

--- TTS.Web/Startup.cs
+++ TTS.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,7 +39,7 @@ namespace TTS.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var connectionString = Configuration["ConnectionString"];
             services.AddAutoMapper(typeof(JobProfile));
             services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(connectionString));
