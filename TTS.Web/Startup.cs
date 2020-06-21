fb1d4521b86f0ba16a42970ad41698a63d97dea7
Aibek Shulembekov <aibek1805@gmail.com>
2020-06-21 14:23:31 +0600

    Added enviroment to docker-compose

--- TTS.Web/Startup.cs
+++ TTS.Web/Startup.cs
@@ -32,7 +32,13 @@ namespace TTS.Web
         public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             _env = env;
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
